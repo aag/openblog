dbb8dc09b79096160e114b7df432bdcd2b199454
Adam Goforth <code@adamgoforth.com>
2016-10-20 23:19:37 +0200

    Improve comment validations
    - Make the count validations more efficient
    - Add an association validation with the post

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,24 +3,32 @@ class Comment < ApplicationRecord
 
   validates :body, presence: { message: 'ist ein Pflichtfeld' }
   validates_format_of :body, with: /(\w+ ){4,}\w/, message: 'ist zu kurz (min. 5 Wörter)'
+  validates_associated :post
 
   validate :max_comments_per_post, :max_comments_per_day
 
   def max_comments_per_post
-    if post.reload.comments.count >= 10
+    # There is no guaranteed ordering of the validations, so we can't assume
+    # that the post has been set.
+    if !post.nil? && post.comments.count >= 10
       errors.add(:body, 'ist über das Limit für diesen Artikel (max. 10 Kommentare)')
     end
   end
 
   def max_comments_per_day
-    beginning_of_day = Time.zone.now.beginning_of_day
-
-    num_comments_today = 0
-    for comment in post.reload.comments do
-      if comment.created_at > beginning_of_day
-        num_comments_today += 1
-      end
+    # There is no guaranteed ordering of the validations, so we can't assume
+    # that the post has been set.
+    if post.nil?
+      return
     end
+
+    beginning_of_day = Time.zone.now.beginning_of_day
+    end_of_day = beginning_of_day.end_of_day
+
+    num_comments_today = post.comments
+                             .where('created_at >= :start_date AND created_at <= :end_date',
+                                   {start_date: beginning_of_day, end_date: end_of_day})
+                             .count
 
     if num_comments_today >= 2
       errors.add(:body, 'ist über das Limit für heute für diesen Artikel (max. 2 Kommentare)')
