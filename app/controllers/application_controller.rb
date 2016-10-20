97fe53038564d9abf8c6f3e7e02cc0a3ee928003
Adam Goforth <code@adamgoforth.com>
2016-10-21 00:18:46 +0200

    Add comment count to header

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  before_action :count_posts
+  before_action :count_posts, :count_comments
 
   protected
 
   def count_posts
     @num_posts = Post.published.count
+  end
+
+  def count_comments
+    @num_comments = Comment.ham.count
   end
 end
